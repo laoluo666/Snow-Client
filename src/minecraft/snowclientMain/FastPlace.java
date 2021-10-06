8b09864bbef95335e3956f5d2fd63772abd37f9f
hhjtqwq <87410002+hhjtqwq@users.noreply.github.com>
2021-10-06 20:28:42 +0800

    Source code update
    SnowClientBase1 Source code

---
+++ src/minecraft/snowclientMain/FastPlace.java
@@ -0,0 +1,12 @@
+package snowclientMain;
+
+import net.minecraft.*;
+import net.minecraft.client.Minecraft;
+
+public class FastPlace {
+	
+	public static void FastPlaceModule() {
+		Minecraft.getMinecraft().rightClickDelayTimer = 0;
+	}
+
+}
---
+++ src/minecraft/snowclientMain/TextLogo.java
@@ -0,0 +1,13 @@
+package snowclientMain;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.GlStateManager;
+
+public class TextLogo {
+	public static void drawString(final double scale,final String text,final float xPos,final float yPos,final int color) {
+		GlStateManager.pushMatrix();
+		GlStateManager.scale(scale, scale, scale);
+		Minecraft.getMinecraft().fontRendererObj.drawStringWithShadow(text, xPos, yPos, color);
+		GlStateManager.popMatrix();
+	}
+}
